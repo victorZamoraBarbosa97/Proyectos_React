60b190e0d050a34565daaf368fe54e56b0bf364d
victor <victor.ernesto.z97@gmail.com>
2023-07-14 18:36:15 -0600

    fix deploy

--- 02-shopping-card/src/App.jsx
+++ 02-shopping-card/src/App.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { Cart } from './components/Cart'
 import { Products } from './components/Products'
-import { Header } from './components/header'
-import { CartProvider } from './context/cart'
-import { useFilters } from './hooks/useFilters'
 import { products as initialProducts } from './mocks/products.json'
+import { Header } from './components/header'
+import { useFilters } from './hooks/useFilters'
+import { Cart } from './components/Cart'
+import { CartProvider } from './context/cart'
+import React from 'react'
 
 function App () {
   const { filterProducts } = useFilters()
   const filteredProducts = filterProducts(initialProducts)
   return (
     <CartProvider>
-      <Header/>
+      <Header />
       <Cart />
       <Products products = { filteredProducts } />
     </CartProvider>
